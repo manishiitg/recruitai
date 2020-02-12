632a76707c4da86cec2502eda28aac637b74bc04
manish <manish.server>
2020-02-12 11:33:14 +0530

    change bucket name

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ MONGO_URI = "mongodb://staging_recruit:staging_recruit@5.9.144.226:27017/staging
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
-RESUME_UPLOAD_BUCKET = "recruitsystemcvpdf"
+RESUME_UPLOAD_BUCKET = "staticrecruitai.excellencetechnologies.in"
 
 GOOGLE_BUCKET_URL = "http://storage.googleapis.com/" + RESUME_UPLOAD_BUCKET + "/"
 
