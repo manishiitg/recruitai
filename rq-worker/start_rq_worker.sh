abd7bfd93b1454b8929a881899da9fa244431876
manish <manish>
2020-02-24 14:21:21 +0530

    docker compoeted

---
+++ rq-worker/start_rq_worker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Need to get PIP_PACKAGES from environ and install
+
+if [ ${PIP_PACKAGES} != 'none' ]; then
+    for i in $(echo ${PIP_PACKAGES} | sed 's/,/ /g'); do
+        pip3 install $i
+    done
+fi
+
+# If there is a requirements file, install that
+
+if [ ${PIP_REQUIREMENTS} != 'none' ]; then
+    pip3 install -r ${PIP_REQUIREMENTS}
+fi
+
+cat /etc/supervisor/conf.d/rqworker.conf.j2 | python -c 'import os;import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' > /etc/supervisor/conf.d/rqworker.conf
+
+supervisord -n
