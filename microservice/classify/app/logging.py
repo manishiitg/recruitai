2eb9e53d178acfc753dbc2435c549a91791a8e7d
manish <manish>
2020-02-25 23:10:20 +0530

    fix

--- microservice/classify/app/logging.py
+++ microservice/classify/app/logging.py
@@ -1,11 +1,11 @@
 import logging
 import sys
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 
