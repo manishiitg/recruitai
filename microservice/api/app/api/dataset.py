97ef6c8fb0d13f472898927294f1d8655a1ce461
manish <manish>
2020-03-03 10:45:53 +0530

    distil bert

---
+++ microservice/api/app/api/dataset.py
@@ -0,0 +1,28 @@
+from app.logging import logger
+from app import token
+from flask import (
+    Blueprint, flash, jsonify, abort, request
+)
+
+from bson.objectid import ObjectId
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity, get_current_user, jwt_refresh_token_required,
+    verify_jwt_in_request
+)
+
+from app.publisher.datasync import sendMessage
+
+bp = Blueprint('dataset', __name__, url_prefix='/dataset')
+
+@bp.route('/issue', methods=['POST'])
+def candidate(id):
+
+    issuetype = request.json.get('type', None)
+
+    # issuetype are NER, CLASSIFY, EMAILCLASSIFY, IMAGEVIZ
+
+    if issuetype == "NER":
+        
+
+    return jsonify(""), 200
