42d3b97189c861f0384e82b37b86947aa7932255
manish <manish>
2020-03-16 12:37:08 +0530

    fixes

--- microservice/datasync/app/process.py
+++ microservice/datasync/app/process.py
@@ -51,7 +51,11 @@ def process(findtype = "", mongoid = None):
     for row in ret:
         row["_id"] = str(row["_id"])
         logger.info(row["_id"])
-        job_profile_id = row["job_profile_id"]
+        if "job_profile_id" in row:
+            job_profile_id = row["job_profile_id"]
+        else:
+            job_profile_id = None
+
         cvParsedInfo = row["cvParsedInfo"]
         finalLines = []
         for page in cvParsedInfo["newCompressedStructuredContent"]:
@@ -62,10 +66,11 @@ def process(findtype = "", mongoid = None):
 
         r.set(row["_id"]  , json.dumps(row,default=json_util.default))
 
-        if job_profile_id not in job_profile_map:
-            job_profile_map[job_profile_id] = []
+        if job_profile_id is not None:
+            if job_profile_id not in job_profile_map:
+                job_profile_map[job_profile_id] = []
 
-        job_profile_map[job_profile_id].append(row)
+            job_profile_map[job_profile_id].append(row)
 
         if len(finalLines) == 0:
             finalLines = [
@@ -75,18 +80,24 @@ def process(findtype = "", mongoid = None):
             ]    
         
         
-        t = Thread(target=addToSearch, args=(row["_id"],finalLines,{}))
-        t.start()
-        t.join() 
-        # no need of tread or async way so that we can reuse connection for pika
-        # if we use threads then many threads start even before connection is created 
-        # so they create multiple connections
-        threads.append(t)
+        if findtype == "syncCandidate":
+            t = Thread(target=addToSearch, args=(row["_id"],finalLines,{}))
+            t.start()
+            t.join() 
+
+            # this is very very slow for full job profile 
+            # need to add bulk to search or something
+
+            # no need of tread or async way so that we can reuse connection for pika
+            # if we use threads then many threads start even before connection is created 
+            # so they create multiple connections
+            threads.append(t)
         
     if findtype != "syncCandidate":
-        for job_profile_id in job_profile_map:
-            ret = r.set("job_" + job_profile_id  , json.dumps(job_profile_map[job_profile_id] , default=json_util.default))
-            logger.info(ret)
+        if job_profile_id is not None:
+            for job_profile_id in job_profile_map:
+                ret = r.set("job_" + job_profile_id  , json.dumps(job_profile_map[job_profile_id] , default=json_util.default))
+                logger.info(ret)
 
 
     for t in threads:
