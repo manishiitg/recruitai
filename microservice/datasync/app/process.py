02b858b094081c1fe74cebc060b362c90d7faae5
manish <manish>
2020-03-21 12:06:02 +0530

    fix

--- microservice/datasync/app/process.py
+++ microservice/datasync/app/process.py
@@ -3,8 +3,6 @@ from threading import Thread
 
 from app.publishsearch import sendBlockingMessage
 
-from bson import json_util
-
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
@@ -20,8 +18,33 @@ db = client[os.environ.get("RECRUIT_BACKEND_DATABASE" , "hr_recruit_dev")]
 import redis
 r = redis.Redis(host=os.environ.get("REDIS_HOST","redis"), port=os.environ.get("REDIS_PORT",6379), db=0)
 
+def moveKey(candidate_id, from_key, to_key):
+    from_job_profile_data = r.get(from_key)
+    from_job_profile_data = json.loads(from_job_profile_data)
 
+    to_job_profile_data = r.get(to_key)
+    to_job_profile_data = json.loads(to_job_profile_data)
 
+    del from_job_profile_data[candidate_id]
+
+    row = db.emailStored.find_one({ 
+            "_id" : ObjectId(candidate_id) } , 
+            {"body": 0}
+        )
+    if row:
+        to_job_profile_data[candidate_id] = row
+        r.set(candidate_id  , json.dumps(row,default=json_util.default))
+        to_job_profile_data[candidate_id] = row
+
+    r.set(from_key  , json.dumps(from_job_profile_data , default=json_util.default))
+    r.set(to_key  , json.dumps(to_job_profile_data , default=json_util.default))
+
+def classifyMoved(candidate_id, from_id, to_id):
+    moveKey(candidate_id, "classify_" + from_id, "classify_" + to_id)
+
+def syncJobProfileChange(candidate_id, from_id, to_id):
+    moveKey(candidate_id, "job_" + from_id, "job_" + to_id)
+    
 def process(findtype = "", mongoid = None):
     threads = []
     # "job_profile_id": mongoid
@@ -50,6 +73,8 @@ def process(findtype = "", mongoid = None):
 
 
     job_profile_map = {}
+    candidate_map = {}
+
     for row in ret:
         row["_id"] = str(row["_id"])
         logger.info(row["_id"])
@@ -64,15 +89,25 @@ def process(findtype = "", mongoid = None):
             for pagerow in cvParsedInfo["newCompressedStructuredContent"][page]:
                 finalLines.append(pagerow["line"])
 
+        candidate_label = None
+        if "candidateClassify" in row:
+            if "label" in row["candidateClassify"]:
+                candidate_label = row["candidateClassify"]["label"]
+                if str(candidate_label) == "False":
+                    candidate_label = None
+                if candidate_label:
+                    if candidate_label not in candidate_map:
+                        candidate_map[candidate_label] = {}
 
+                    candidate_map[candidate_label][row["_id"]] = row
 
         r.set(row["_id"]  , json.dumps(row,default=json_util.default))
 
         if job_profile_id is not None:
             if job_profile_id not in job_profile_map:
-                job_profile_map[job_profile_id] = []
+                job_profile_map[job_profile_id] = {}
 
-            job_profile_map[job_profile_id].append(row)
+            job_profile_map[job_profile_id][row["_id"]] = row
 
         if len(finalLines) == 0:
             finalLines = [
@@ -83,9 +118,17 @@ def process(findtype = "", mongoid = None):
         
         
         if findtype == "syncCandidate":
-            t = Thread(target=addToSearch, args=(row["_id"],finalLines,{}))
-            t.start()
-            t.join() 
+            job_profile_data_existing = r.get("job_" + job_profile_id)
+
+            job_profile_data_now = json.dumps(row,default=json_util.default)
+
+            if job_profile_data_existing != job_profile_data_now:
+                logger.info("add to searhch")
+                t = Thread(target=addToSearch, args=(row["_id"],finalLines,{}))
+                t.start()
+                t.join() 
+            else:
+                logger.info("skip add to searhch")
 
             # this is very very slow for full job profile 
             # need to add bulk to search or something
@@ -93,17 +136,46 @@ def process(findtype = "", mongoid = None):
             # no need of tread or async way so that we can reuse connection for pika
             # if we use threads then many threads start even before connection is created 
             # so they create multiple connections
-            threads.append(t)
+            # threads.append(t)
+
+            if job_profile_id is not None:
+                job_profile_data = r.get("job_" + job_profile_id)
+                job_profile_data = json.loads(job_profile_data)
+
+                if row["_id"] in job_profile_data:
+                    job_profile_data[row["_id"]] = row
+                
+            if candidate_label is not None:
+                logger.info(candidate_label)
+                candidate_data = r.get("classify_" + candidate_label)
+                candidate_data = json.loads(candidate_data)
+
+                if row["_id"] in candidate_data:
+                    candidate_data[row["_id"]] = row
+
+
+                # there is one case here. if candidate changes job profile, we need to remove it from previous job as well
+                # this will be handled via a seperate api
+
+    
         
-    if findtype != "syncCandidate":
+
+    if findtype == "syncJobProfile":
         if job_profile_id is not None:
             for job_profile_id in job_profile_map:
                 ret = r.set("job_" + job_profile_id  , json.dumps(job_profile_map[job_profile_id] , default=json_util.default))
                 logger.info(ret)
+    
+    if findtype == "full":
+        for job_profile_id in job_profile_map:
+            r.set("job_" + job_profile_id  , json.dumps(job_profile_map[job_profile_id] , default=json_util.default))
+
+        for candidate_label in candidate_map:
+            r.set("classify_" + candidate_label  , json.dumps(candidate_map[candidate_label] , default=json_util.default))
 
 
-    for t in threads:
-        t.join()
+    # for t in threads:
+    #     t.join()
 
         
 
--- microservice/filter/app/filter/start.py
+++ microservice/filter/app/filter/start.py
@@ -1,6 +1,7 @@
 from app.logging import logger
 from pymongo import MongoClient
 
+from app.filter.util import parse_experiance_years, matchCourse, designation, location, cleanLocation, cleanWorkExp
 
 import redis
 
@@ -20,14 +21,77 @@ def start(mongoid, filter_type="job_profile"):
 
     db = initDB()
 
+
+    data = [] 
+
     if filter_type == "job_profile":
-        ret = db.emailStored.find({
-            'job_profile_id': mongoid
-        })
+        data = r.get("job_" + mongoid)
+        data = json.loads(data)
+        
 
     elif filter_type == "candidate":
-        ret = db.emailStored.find({
-            'candidateClassify': 
-        })
+        data = r.get("classify_" + mongoid)
+        data = json.loads(data)
+
+    wrkExpList = []
+    gpeList = []
+
+    designation_map = {}
+    location_map = {}
+    for row in data:
+        if "finalEntity" in row["cvParsedInfo"]:    
+            if "ExperianceYears" in row["cvParsedInfo"]["finalEntity"]:
+                ExperianceYears = row["cvParsedInfo"]["finalEntitey"]["ExperianceYears"]
+                # print(ExperianceYears["obj"])
+
+                days, _, _ =  parse_experiance_years(ExperianceYears["obj"])
+
+            if "EducationDegree" in row["cvParsedInfo"]["finalEntity"]:
+                EducationDegree = row["cvParsedInfo"]["finalEntity"]["EducationDegree"]
+                education = matchCourse(EducationDegree["obj"])
+
+            if "wrkExp" in row["cvParsedInfo"]["finalEntity"]:
+                wrkExp = row["cvParsedInfo"]["finalEntity"]["wrkExp"]
+                if wrkExp:
+                    for w in wrkExp:
+                        if "Designation" in w[0]:
+                            wrkExpList.append(w[0]["Designation"])
+                            designation_map[str(row["_id"])] = w[0]["Designation"]
+
+            if "GPE" in row["cvParsedInfo"]["finalEntity"]:
+                GPE = row["cvParsedInfo"]["finalEntity"]["GPE"]
+                if GPE:
+                    gpeList.append(GPE["obj"])
+                    designation_map[str(row["_id"])] = GPE["obj"]
+
+    wrkCombineList = designation(wrkExpList)
+    gpeCombineList = location(gpeList)
+
+    for key in designation_map:
+        desg = designation_map[key]
+        desg = cleanWorkExp(desg)
+        found = False
+        for key in wrkCombineList:
+
+            if desg == key:
+                found = True
+                break
+
+            data = wrkCombineList[key]
+            count = data["count"]
+            merge = data["merge"]
+
+            for m in merge:
+                if desg == m:
+                    found = True
+                    break
+
+        if not found:
+            logger.info("not found for %s", desg)
+
+    
+
+
+
 
     pass
