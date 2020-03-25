c9ccb80ba2dd2711a3ebea233778f5a7149dabef
manish <manish>
2020-03-25 08:39:06 +0530

    timeout and process killing in imagemq

--- microservice/image/app/resumeutil.py
+++ microservice/image/app/resumeutil.py
@@ -1,5 +1,5 @@
 from app.image.start import processAPI
-from app.config import RESUME_UPLOAD_BUCKET, BASE_PATH, GOOGLE_BUCKET_URL, RECRUIT_BACKEND_DB, RECRUIT_BACKEND_DATABASE
+from app.config import RESUME_UPLOAD_BUCKET, BASE_PATH, GOOGLE_BUCKET_URL
 from app.logging import logger
 from app.config import storage_client
 from pathlib import Path
@@ -18,11 +18,12 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 import traceback
-from subprocess import CalledProcessError
+from subprocess import CalledProcessError , TimeoutExpired
 
 import sys
 
 import time
+import psutil
 
 from google.api_core.exceptions import NotFound
 
@@ -35,14 +36,39 @@ def initDB():
 
     return db
 
+def deleteDirContents(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            logger.info('Failed to delete %s. Reason: %s' , file_path, e)
+
+def killlibreoffice():
+    for proc in psutil.process_iter():
+        # check whether the process name matches
+        if "soffice" in proc.name():
+            proc.kill()
+
 def fullResumeParsing(filename, mongoid=None, skills = None):
+    
+    killlibreoffice()
+
 
     timer = time.time()
+
+    
+
+    dest = BASE_PATH + "/../temp"
+    deleteDirContents(dest)
+    deleteDirContents(BASE_PATH + "/../cvreconstruction")
 
     bucket = storage_client.bucket(RESUME_UPLOAD_BUCKET)
     blob = bucket.blob(filename)
 
-    dest = BASE_PATH + "/../temp"
     Path(dest).mkdir(parents=True, exist_ok=True)
     filename, file_extension = os.path.splitext(filename)
 
@@ -73,11 +99,17 @@ def fullResumeParsing(filename, mongoid=None, skills = None):
         try:
             logger.info('libreoffice --headless --convert-to pdf ' + inputFile + " --outdir  " + dest)
             x = subprocess.check_call(
-                ['libreoffice --headless --convert-to pdf ' + inputFile + " --outdir  " + dest], shell=True, timeout=30)
+                ['libreoffice --headless --convert-to pdf ' + inputFile + " --outdir  " + dest], shell=True, timeout=60)
             logger.info(x)
         except CalledProcessError as e:
             logger.critical(str(e))
             traceback.print_exc(file=sys.stdout)
+            # os.remove(inputFile) 
+            return {"error" : str(e)}
+        except TimeoutExpired as e:
+            logger.critical(str(e))
+            traceback.print_exc(file=sys.stdout)
+            # os.remove(inputFile) 
             return {"error" : str(e)}
 
         if os.path.exists(os.path.join(dest, filename)):
@@ -116,7 +148,7 @@ def fullResumeParsing(filename, mongoid=None, skills = None):
         })
         timer = time.time()
         
-    shutil.rmtree(os.path.join(dest, cvdir) , ignore_errors = False) 
+    shutil.rmtree(os.path.join(dest, cvdir)) 
     if os.path.exists(os.path.join(dest, org_cv_filename)):
         os.remove(os.path.join(dest, org_cv_filename)) 
     if os.path.exists(os.path.join(dest, filename)):
