fa128e1ea395b9fe831a31a4628b8212028f13b7
manish <manish>
2020-03-29 23:49:05 +0530

    summary and pic

---
+++ microservice/summary/app/logging.py
@@ -0,0 +1,12 @@
+import logging
+import sys
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.CRITICAL)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
