3673e381e0bacb538bbfc68cd5357feda7136552
manish <manish>
2020-03-30 11:22:32 +0530

    fix

--- microservice/stats/app/logging.py
+++ microservice/stats/app/logging.py
@@ -1,12 +1,12 @@
 import logging
 import sys
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.CRITICAL)
 logger = logging.getLogger(__name__)
 
-handler = logging.StreamHandler(sys.stderr)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
+# handler = logging.StreamHandler(sys.stderr)
+# handler.setLevel(logging.CRITICAL)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# handler.setFormatter(formatter)
 
-logger.addHandler(handler)
+# logger.addHandler(handler)
