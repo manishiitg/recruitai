4442cc35f4e5c4a82f441ca33af0424f9baf16fc
manish <serverryzen@gmail.com>
2020-05-04 13:24:46 +0530

    recruit fixes

--- microservice/resume/app/logging.py
+++ microservice/resume/app/logging.py
@@ -1,12 +1,25 @@
 import logging
 import sys
 
+import os
+hostname = os.environ['HOSTNAME']
+
+class ContextFilter(logging.Filter):
+    """
+    This is a filter which injects contextual information into the log.
+    """
+    def filter(self, record):
+        record.hostname = hostname
+        return True
+
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.CRITICAL)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(hostname)s - %(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
+logger.addFilter(ContextFilter())
