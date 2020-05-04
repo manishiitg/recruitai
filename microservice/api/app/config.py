24f5104dbe6c78a117b574b0dfb6a248858a2c19
manish <serverryzen@gmail.com>
2020-05-04 16:28:03 +0530

    commit

--- microservice/api/app/config.py
+++ microservice/api/app/config.py
@@ -1,6 +1,7 @@
 import os 
 from app.logging import logger
 
+from google.cloud import storage
 
 BATCH_PROCESSING_DELAY = os.getenv("BATCH_PROCESSING_DELAY", "60")
 BATCH_PROCESSING_DELAY = int(BATCH_PROCESSING_DELAY)
@@ -19,3 +20,5 @@ else:
 
 # if true, this will parse cv instantly instead of rq worker
 logger.info("is dev? %s",IS_DEV)
+
+storage_client = storage.Client.from_service_account_json('/workspace/RecruitAI.json')
