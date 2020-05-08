292505f85976b3ca97502010946003539b335a86
manish <serverryzen@gmail.com>
2020-05-08 10:54:18 +0530

    stats

--- microservice/stats/app/logging.py
+++ microservice/stats/app/logging.py
@@ -1,11 +1,11 @@
 import logging
 import sys
 
-logging.basicConfig(level=logging.CRITICAL)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 handler = logging.StreamHandler(sys.stderr)
-handler.setLevel(logging.CRITICAL)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 
