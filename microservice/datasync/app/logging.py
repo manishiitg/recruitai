7270ad997ed9c4fe88e55b7071a9dd0c5fa9cef3
manish <serverryzen@gmail.com>
2020-05-15 14:41:48 +0530

    bug fixes

--- microservice/datasync/app/logging.py
+++ microservice/datasync/app/logging.py
@@ -5,7 +5,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 handler = logging.StreamHandler(sys.stderr)
-handler.setLevel(logging.CRITICAL)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 
