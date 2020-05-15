c83c47fc933b3c9dd7ab8068d8111eda8e762c59
manish <serverryzen@gmail.com>
2020-05-15 14:41:48 +0530

    bug fixes

--- microservice/filter/app/main.py
+++ microservice/filter/app/main.py
@@ -72,7 +72,7 @@ def thread_task( ch, method_frame, properties, body):
             # job_profile, candidate, full_map
             if action == 'fetch':
                 ret = fetch(fetch_id, fetch_type, tags, page, limit, on_ai_data, filter, account_name, account_config)
-                # logger.info(ret)
+                logger.info(ret)
                 add_threadsafe_callback(ch, method_frame,properties,ret)
             else:
                 ret = index(fetch_id, fetch_type, account_name, account_config)
