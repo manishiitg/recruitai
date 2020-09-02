4e14e73d96671a3a5595b784d8ab1c8f9423fc2c
manish <serverryzen@gmail.com>
2020-09-02 16:59:32 +0530

    ner data

--- training/util/get_labels_and_remove_labels.py
+++ training/util/get_labels_and_remove_labels.py
@@ -15,7 +15,7 @@ def get_labels(file):
     return set(list(result))
 
 
-def get_file_after_remove_labels(file, labels_to_remove=[]):
+def get_file_after_remove_labels(file, labels_to_remove=[], file2=False):
     newfile = "label_remove_email_dob" + file
     with open(newfile, "w") as myfile2:
         with open(file, "r") as myfile:
@@ -34,6 +34,23 @@ def get_file_after_remove_labels(file, labels_to_remove=[]):
 
                     myfile2.write(val + "  " + label + "\n")
 
+        if file2:
+            with open(file2, "r") as myfile:
+                nerTestLines = myfile.read()
+                nerTestLines = nerTestLines.splitlines()
+
+                for line in nerTestLines:
+                    if len(line.strip()) == 0:
+                        myfile2.write("\n")
+                    else:
+                        val = line.split("  ")[0].strip()
+                        label = line.split("  ")[1].strip()
+
+                        if label in labels_to_remove:
+                            label = "O"
+
+                        myfile2.write(val + "  " + label + "\n")
+
     myfile2.close()
     return newfile
 
@@ -41,10 +58,10 @@ def get_file_after_remove_labels(file, labels_to_remove=[]):
 labels = get_labels("ner-train-v2.txt")
 print(labels)
 
-filename = get_file_after_remove_labels("ner-train-v2.txt", ["Skills",'LANGUAGE'])
-labels1 = get_labels(filename)
-print(labels1)
+get_file_after_remove_labels(
+    "ner-train-v2.txt", ["Skills", 'LANGUAGE'], "ner-final-train.txt")
 
-filename = get_file_after_remove_labels("ner-full-v2.txt", ["Skills",'LANGUAGE'])
-filename = get_file_after_remove_labels("ner-test-v2.txt", ["Skills",'LANGUAGE'])
-filename = get_file_after_remove_labels("ner-dev-v2.txt", ["Skills",'LANGUAGE'])
+get_file_after_remove_labels(
+    "ner-test-v2.txt", ["Skills", 'LANGUAGE'], "ner-final-test.txt")
+get_file_after_remove_labels(
+    "ner-dev-v2.txt", ["Skills", 'LANGUAGE'], "ner-final-dev.txt")
