051a216f096b9dc48cc8a570f4733c8e46c4294f
manish <serverryzen@gmail.com>
2020-09-13 11:02:48 +0530

    more fixes

--- microservice/api/app/publisher/filter.py
+++ microservice/api/app/publisher/filter.py
@@ -2,6 +2,8 @@ from app.logging import logger
 import os
 import pika
 import json
+import time
+
 ROUTING_KEY = 'rpc.filter.queue'
 EXCHANGE = ""
 
@@ -23,16 +25,26 @@ def handle(channel, method, properties, body):
 
 def sendBlockingMessage(obj):
 
+    start_time = time.time()
+    
     connection, channel = getConnection()
+
+    print("--- %s get connection ---" % (time.time() - start_time))
+
 
     # with connection, channel:
     message = json.dumps(obj)
     next(channel.consume(queue="amq.rabbitmq.reply-to", auto_ack=True,
                         inactivity_timeout=0.1))
+
+    print("--- %s channel consume ---" % (time.time() - start_time))
+
     channel.basic_publish(
         exchange=EXCHANGE, routing_key=ROUTING_KEY, body=message.encode(),
-        properties=pika.BasicProperties(reply_to="amq.rabbitmq.reply-to",expiration='300'))
+        properties=pika.BasicProperties(reply_to="amq.rabbitmq.reply-to",expiration='3000'))
     logger.info("sent: %s", message)
+    print("--- %s message sent ---" % (time.time() - start_time))
+
 
     for (method, properties, body) in channel.consume(
             queue="amq.rabbitmq.reply-to", auto_ack=True,inactivity_timeout=60):
