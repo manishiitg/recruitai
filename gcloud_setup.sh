519f35b8b10bdd37001e15ac2e42254c299329e4
manish <serverryzen@gmail.com>
2020-09-17 16:37:08 +0530

    fix

--- gcloud_setup.sh
+++ gcloud_setup.sh
@@ -31,16 +31,15 @@ sudo mkdir -p /var/log/recruitai
 
 cd /home/jupyter/drive/
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 sudo pip3 install git+https://github.com/beehiveai/compose.git
-
+sudo pip3 install -U six
 cd /home/jupyter/drive/recruitai
 
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
-sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
