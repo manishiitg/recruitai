46213335093d43aa5cc9d664cc319bb9c3ce7fa7
manish <serverryzen@gmail.com>
2020-09-18 14:12:11 +0530

    fix

--- microservice/gpucloud/app/start.sh
+++ microservice/gpucloud/app/start.sh
@@ -6,4 +6,4 @@
 # echo "$1" >&1
 # echo "x$2x" >&1
 
-gcloud beta compute instances create $1 --zone=$2 --image-family=pytorch-latest-gpu --image-project=deeplearning-platform-release --maintenance-policy=TERMINATE --accelerator="type=nvidia-tesla-t4,count=1" --metadata="install-nvidia-driver=True" --machine-type="n1-standard-4" --boot-disk-type=pd-ssd --metadata-from-file startup-script=/workspace/app/gcloud_setup_summary.sh --scopes=logging-write,compute-rw --create-disk name=torchsummary,size=100GB,type=pd-ssd,auto-delete=yes --preemptible --format=json >&1
+gcloud beta compute instances create $1 --zone=$2 --image-family=common-cu101 --image-project=deeplearning-platform-release --maintenance-policy=TERMINATE --accelerator="type=nvidia-tesla-t4,count=1" --metadata="install-nvidia-driver=True" --machine-type="n1-standard-4" --boot-disk-type=pd-ssd --metadata-from-file startup-script=/workspace/app/gcloud_setup_summary.sh --scopes=logging-write,compute-rw,cloud-platform --create-disk name=torchsummary,size=100GB,type=pd-ssd,auto-delete=yes --preemptible --format=json >&1
