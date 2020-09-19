7731d1bbe12632f82f9fec90ab814c2afb550b33
manish <serverryzen@gmail.com>
2020-09-19 17:57:29 +0530

    fix

--- microservice/summary/app/config.py
+++ microservice/summary/app/config.py
@@ -4,8 +4,11 @@ BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 IN_COLAB = False
 
 
-import os
-from google.cloud import storage
+try:
+    from google.cloud import storage
 
-storage_client = storage.Client.from_service_account_json(
-            BASE_PATH + '/RecruitAI.json')
+    storage_client = storage.Client.from_service_account_json(
+        BASE_PATH + '/RecruitAI.json')
+
+except Exception as e:
+    pass
