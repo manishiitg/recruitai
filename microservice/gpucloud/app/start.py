e612fa691aa037ef3dcd86f49a05fe621e8fd128
manish <serverryzen@gmail.com>
2020-09-21 12:12:13 +0530

    fix

--- microservice/gpucloud/app/start.py
+++ microservice/gpucloud/app/start.py
@@ -1,3 +1,4 @@
+from sendgrid.helpers.mail import *
 import requests
 from requests.auth import HTTPBasicAuth
 from app.logging import logger as LOGGER
@@ -9,7 +10,7 @@ import json
 import shlex
 from datetime import datetime, timezone, timedelta
 import random
-
+import sendgrid
 
 amqp_url_base = os.getenv('RABBIT_API_URL')
 RabbitMQLOGIN = os.getenv("RABBIT_LOGIN")
@@ -20,9 +21,12 @@ running_computes = []
 
 max_instances_to_run_together = 1
 
+run_combined_gpu = True
+
 
 is_process_running = False
 last_process_running_time = 0
+
 
 def queue_process():
 
@@ -31,6 +35,7 @@ def queue_process():
     global running_instance_check
     global running_computes
     global last_process_running_time
+    global run_combined_gpu
 
     if is_process_running:
         if (time.time() - last_process_running_time) < 1 * 60 * 60:
@@ -44,17 +49,24 @@ def queue_process():
     max_process_to_kill_gpu = 1
 
     queues = get_queues()
-    
-    for queue_type in ["summary", "picture"]:
+
+    for queue_type in ["summary", "picture", 'all']:
+        if run_combined_gpu:
+            if queue_type != "all":
+                continue
+        else:
+            if queue_type == "all":
+                continue
+
         if queue_type in queues:
 
             type_instance_name = "torch" + queue_type
             LOGGER.critical("checking for queue type %s", type_instance_name)
             if int(queues[queue_type]['in_process']) > min_process_to_start_gpu:
-                
 
                 instance_status = check_compute_running(type_instance_name)
-                LOGGER.critical("number of running %s instances %s", queue_type,  len(instance_status))
+                LOGGER.critical("number of running %s instances %s",
+                                queue_type,  len(instance_status))
                 if len(instance_status) > 0:
                     for instance in instance_status:
                         is_any_torch_running = instance["is_any_torch_running"]
@@ -62,45 +74,56 @@ def queue_process():
                         running_instance_name = instance["running_instance_name"]
                         running_instance_zone = instance["zone"]
 
-                        LOGGER.critical("instance status for type %s is_any_torch_running %s is_torch_responding %s, running_instance_name %s ", type_instance_name, is_any_torch_running, is_torch_responding, running_instance_name)
-                        if is_any_torch_running:    
+                        LOGGER.critical("instance status for type %s is_any_torch_running %s is_torch_responding %s, running_instance_name %s ",
+                                        type_instance_name, is_any_torch_running, is_torch_responding, running_instance_name)
+                        if is_any_torch_running:
                             if not is_torch_responding:
                                 if running_instance_name not in running_instance_check:
-                                    LOGGER.critical("picking starting %s", instance["created_at"])
-                                    running_instance_check[running_instance_name] = time.time() - instance["created_at"]
-
-
+                                    LOGGER.critical(
+                                        "picking starting %s", instance["created_at"])
+                                    running_instance_check[running_instance_name] = time.time(
+                                    ) - instance["created_at"]
 
                                 if running_instance_name in running_instance_check:
                                     prev_check_time = running_instance_check[running_instance_name]
                                     time_passed = time.time() - prev_check_time
                                     # it takes 5-10min for gpu to start its processing
-                                    LOGGER.critical("time passed %s for instance %s", time_passed, running_instance_name)
+                                    LOGGER.critical(
+                                        "time passed %s for instance %s", time_passed, running_instance_name)
 
                                     max_time_passed = 60 * 10
                                     if queue_type == "picture":
                                         max_time_passed = 60 * 15
+                                    if queue_type == "all":
+                                        max_time_passed = 60 * 20
 
                                     if time_passed > max_time_passed:
-                                        LOGGER.critical("some wrong majorly so deleting instance %s as time passed %s", running_instance_name, time_passed)
-                                        delete_instance(running_instance_name, running_instance_zone)
+                                        LOGGER.critical(
+                                            "some wrong majorly so deleting instance %s as time passed %s", running_instance_name, time_passed)
+                                        delete_instance(
+                                            running_instance_name, running_instance_zone, "vm not responding even after " + max_time_passed)
                                         del running_instance_check[running_instance_name]
 
                                 else:
-                                    LOGGER.critical("its running but not responsive which is not good! check again")
-                                    running_instance_check[running_instance_name] = time.time()
+                                    LOGGER.critical(
+                                        "its running but not responsive which is not good! check again")
+                                    running_instance_check[running_instance_name] = time.time(
+                                    )
                             else:
                                 if running_instance_name in running_instance_check:
                                     del running_instance_check[running_instance_name]
-                                LOGGER.critical("already gpu %s running so nothing else to do", running_instance_name)
+                                LOGGER.critical(
+                                    "already gpu %s running so nothing else to do", running_instance_name)
                 else:
                     LOGGER.critical("not gpu running need to start")
                     start_compute_preementable(type_instance_name, queue_type)
             else:
-                LOGGER.critical("%s has less than %s no need to start gpu %s", queue_type, min_process_to_start_gpu ,queues["summary"])
+                LOGGER.critical("%s has less than %s no need to start gpu %s",
+                                queue_type, min_process_to_start_gpu, queues["summary"])
 
                 instance_status = check_compute_running(type_instance_name)
-                LOGGER.critical("number of running instances %s", len(instance_status))
+                LOGGER.critical("number of running instances %s",
+                                len(instance_status))
                 if len(instance_status) > 0:
                     if int(queues[queue_type]['in_process']) <= max_process_to_kill_gpu:
                         LOGGER.critical("killing running gpus as no need")
@@ -109,8 +132,10 @@ def queue_process():
                             is_torch_responding = instance["is_torch_responding"]
                             running_instance_name = instance["running_instance_name"]
                             running_instance_zone = instance["zone"]
-                            LOGGER.critical("instance status for type %s is_any_torch_running %s is_torch_responding %s, running_instance_name %s ", type_instance_name, is_any_torch_running, is_torch_responding, running_instance_name)
-                            delete_instance(running_instance_name, running_instance_zone)
+                            LOGGER.critical("instance status for type %s is_any_torch_running %s is_torch_responding %s, running_instance_name %s ",
+                                            type_instance_name, is_any_torch_running, is_torch_responding, running_instance_name)
+                            delete_instance(running_instance_name,
+                                            running_instance_zone, "work completed")
                             if running_instance_name in running_instance_check:
                                 del running_instance_check[running_instance_name]
                     else:
@@ -121,6 +146,7 @@ def queue_process():
 
     is_process_running = False
 
+
 def get_queues():
     res = requests.get(amqp_url_base + "/api/queues", verify=False,
                        auth=HTTPBasicAuth(RabbitMQLOGIN.split(":")[0], RabbitMQLOGIN.split(":")[1]))
@@ -129,6 +155,7 @@ def get_queues():
 
     mq_status = {}
     running_process = 0
+    total = 0
     for queue in queues:
         if queue["name"] == "resume" or queue["name"] == "picture" or queue["name"] == "summary":
 
@@ -139,9 +166,15 @@ def get_queues():
                     "in_process": queue["messages_unacknowledged_ram"] + queue["messages_ready"]
                 }
                 running_process += int(queue["messages_unacknowledged_ram"])
+                total += int(queue["messages_unacknowledged_ram"]) + int(queue["messages_ready"])
 
     LOGGER.critical(mq_status)
     # print(running_process)
+
+    mq_status["all"] = {
+        "consumers": 0,
+        "in_process": total
+    }
 
     return mq_status
 
@@ -183,7 +216,6 @@ def check_compute_running(instance_name):
         running_instance_name = ""
         LOGGER.info("name %s, status %s", vm['name'], vm['status'])
 
-        
         if instance_name in vm['name']:
             is_any_torch_running = True
 
@@ -194,11 +226,12 @@ def check_compute_running(instance_name):
             actual_time_stamp = vm["creationTimestamp"][:-6]
             # print(actual_time_stamp)
 
-            created_at = datetime.strptime(vm["creationTimestamp"], '%Y-%m-%dT%H:%M:%S.%f%z')
-            print("created_at" , created_at)
+            created_at = datetime.strptime(
+                vm["creationTimestamp"], '%Y-%m-%dT%H:%M:%S.%f%z')
+            print("created_at", created_at)
             now = datetime.now(timezone(timedelta(hours=5, minutes=30)))
             difference = now - created_at
-            print("now" , now)
+            print("now", now)
             print("difference", difference.total_seconds())
             # process.exit()
 
@@ -213,16 +246,16 @@ def check_compute_running(instance_name):
                 vminstance = vm["networkInterfaces"]
 
             if "accessConfigs" in vminstance:
-                
+
                 if isinstance(vminstance['accessConfigs'], list):
-                    accessConfig  = vminstance['accessConfigs'][0]
+                    accessConfig = vminstance['accessConfigs'][0]
                 else:
-                    accessConfig  = vminstance['accessConfigs']
+                    accessConfig = vminstance['accessConfigs']
 
                 if "natIP" in accessConfig:
                     ip = accessConfig["natIP"]
             # print()
-            
+
             for connection in connections:
                 # LOGGER.critical("%s == %s", connection["peer_host"],ip)
                 if connection["peer_host"] == ip:
@@ -230,18 +263,19 @@ def check_compute_running(instance_name):
                     is_torch_responding = True
                     break
 
-            LOGGER.critical("vm ip %s is_any_torch_running %s is_torch_responding %s", ip, is_any_torch_running, is_torch_responding)
+            LOGGER.critical("vm ip %s is_any_torch_running %s is_torch_responding %s",
+                            ip, is_any_torch_running, is_torch_responding)
             compute = {
-                "is_any_torch_running" : is_any_torch_running,
-                "is_torch_responding" : is_torch_responding,
-                "running_instance_name" : running_instance_name,
-                "zone" : vm["zone"],
-                "created_at" : difference.total_seconds()
+                "is_any_torch_running": is_any_torch_running,
+                "is_torch_responding": is_torch_responding,
+                "running_instance_name": running_instance_name,
+                "zone": vm["zone"],
+                "created_at": difference.total_seconds()
             }
             instance_status.append(compute)
             # running_computes.append(compute)
 
-    return instance_status  #is_any_torch_running, is_torch_responding, instance_name
+    return instance_status  # is_any_torch_running, is_torch_responding, instance_name
 
 
 def get_zone_list():
@@ -261,22 +295,29 @@ def get_zone_list():
     # }
     return zone_list
 
+
 def start_compute_preementable(instance_name, queue_type):
     global max_instances_to_run_together
     zones = get_zone_list()
     max_attemps = 10
     started_instance = 0
+    email_subject = "start compute preementable " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    email_content = ""
     for idx, zone in enumerate(zones):
 
-        name = instance_name+ str(idx)
-        LOGGER.critical("trying to start gpu for instance name %s for zone regision %s", name , zone["name"])
+        name = instance_name + str(idx)
+        log = f"trying to start gpu for instance name {name} for zone regision {zone['name']}"
+        email_content += log + "<br/>"
+        LOGGER.critical(log)
         start_compute(name, zone["name"], queue_type)
 
         instance_status = check_compute_running(name)
         if len(instance_status) > 0:
+            email_content += "gpu started so breaking out " + name + "<br/>"
             LOGGER.critical("gpu started so breaking out")
             started_instance += 1
             if started_instance >= max_instances_to_run_together:
+                email_content += "ran max instances to breaking out" + "<br/>"
                 LOGGER.critical("ran max instances to breaking out")
                 break
             else:
@@ -285,6 +326,9 @@ def start_compute_preementable(instance_name, queue_type):
         if idx > max_attemps:
             LOGGER.critical("max attempts reached to start so breaking out")
             break
+
+    sendEmail(email_subject, email_content)
+
 
 def start_compute(instance_name, zone, queue_type):
 
@@ -302,14 +346,16 @@ def start_compute(instance_name, zone, queue_type):
 
         image_family = ["pytorch-latest-gpu", "common-cu101", "common-cu100"]
 
-        image_family_name = random.choice(image_family)  # sometimes nvidia randomly fails
+        # sometimes nvidia randomly fails
+        image_family_name = random.choice(image_family)
 
         # result = subprocess.call(shlex.split(f"./start.sh {instance_name} {zone}"), cwd="/workspace/app")
         if queue_type == "summary":
             result = subprocess.call(shlex.split(f"gcloud beta compute instances create {instance_name} --zone={zone} --image-family={image_family_name} --image-project=deeplearning-platform-release --maintenance-policy=TERMINATE --accelerator type=nvidia-tesla-t4,count=1 --metadata install-nvidia-driver=True --machine-type=n1-standard-4 --boot-disk-type=pd-ssd --metadata-from-file startup-script=/workspace/app/gcloud_setup_summary.sh --scopes=logging-write,compute-rw,cloud-platform --create-disk size=100GB,type=pd-ssd,auto-delete=yes --preemptible --format=json"), stdout=subprocess.PIPE)
         elif queue_type == "picture":
             result = subprocess.call(shlex.split(f"gcloud beta compute instances create {instance_name} --zone={zone} --image-family={image_family_name} --image-project=deeplearning-platform-release --maintenance-policy=TERMINATE --accelerator type=nvidia-tesla-t4,count=1 --metadata install-nvidia-driver=True --machine-type=n1-standard-4 --boot-disk-type=pd-ssd --metadata-from-file startup-script=/workspace/app/gcloud_setup_picture.sh --scopes=logging-write,compute-rw,cloud-platform --create-disk size=100GB,type=pd-ssd,auto-delete=yes --preemptible --format=json"), stdout=subprocess.PIPE)
-
+        else:
+            result = subprocess.call(shlex.split(f"gcloud beta compute instances create {instance_name} --zone={zone} --image-family={image_family_name} --image-project=deeplearning-platform-release --maintenance-policy=TERMINATE --accelerator type=nvidia-tesla-t4,count=1 --metadata install-nvidia-driver=True --machine-type=n1-standard-4 --boot-disk-type=pd-ssd --metadata-from-file startup-script=/workspace/app/gcloud_setup_all.sh --scopes=logging-write,compute-rw,cloud-platform --create-disk size=100GB,type=pd-ssd,auto-delete=yes --preemptible --format=json"), stdout=subprocess.PIPE)
         # LOGGER.critical("stdout", result)
 
         # if result.stderr and len(result.stderr) > 0:
@@ -339,12 +385,18 @@ def start_compute(instance_name, zone, queue_type):
         pass
 
 
-def delete_instance(instance_name, zone):
+def delete_instance(instance_name, zone, reason):
     # if is_any_torch_running:
+    email_subject = "delete instance " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + reason
+    email_content = ""
+
     try:
         LOGGER.critical(" deleting instance")
 
-        result = subprocess.call(shlex.split(f"gcloud beta compute instances delete {instance_name} --zone={zone} --quiet --format=json"), stdout=subprocess.PIPE)
+        
+
+        result = subprocess.call(shlex.split(
+            f"gcloud beta compute instances delete {instance_name} --zone={zone} --quiet --format=json"), stdout=subprocess.PIPE)
         # result = subprocess.run(['gcloud', 'compute', 'instances', 'delete', instance_name,
         #                          "--zone="+zone, '--quiet', '--format="json"'], stdout=subprocess.PIPE)
 
@@ -353,11 +405,34 @@ def delete_instance(instance_name, zone):
 
         if result.stdout and len(result.stdout) > 0:
             LOGGER.critical("stdout %s", result.stdout)
+            email_content = result.stdout
+
         return True
     except Exception as e:
         LOGGER.critical("YYY %s", e)
+        email_content = str(e)
         return False
 
+    sendEmail(email_subject, email_content)
+
+
 checkin_score_scheduler = BackgroundScheduler()
-checkin_score_scheduler.add_job(queue_process, trigger='interval', seconds=1 * 60)
+checkin_score_scheduler.add_job(
+    queue_process, trigger='interval', seconds=1 * 60)
 checkin_score_scheduler.start()
+
+
+def sendEmail(subject, content):
+    
+
+
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("manish@excellencetechnologies.in")
+    to_email = To("manisharies.iitg@gmail.com")
+    subject = subject
+    content = Content("text/plain", content)
+    mail = Mail(from_email, to_email, subject, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    # print(response.status_code)
+    # print(response.body)
+    # print(response.headers)
