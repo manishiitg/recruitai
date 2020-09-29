6e69861ee209d0ff9602ae0a482ac00b465094b6
manish <serverryzen@gmail.com>
2020-09-29 17:58:41 +0530

    fix

--- microservice/gpucloud/app/gcloud_setup_all.sh
+++ microservice/gpucloud/app/gcloud_setup_all.sh
@@ -172,9 +172,9 @@ sudo docker-compose -f docker-compose-gpu-resume.yml -f docker-compose-gpu-pictu
 sudo docker-compose -f docker-compose-gpu-resume.yml -f docker-compose-gpu-picture.yml -f docker-compose-gpu-summary.yml up -d --scale=picturegpu=1 --scale=summarygpu=1 --scale=resumegpu=2 
 
 
-for i in {1..50}
+for i in {1..1000}
 do
-  sleep 1h
+  sleep 30m
   echo "restarting docker $i"
   sudo docker-compose -f docker-compose-gpu-all.yml restart
   
