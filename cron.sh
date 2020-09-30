0d0b11f21a35b391d980945f024a115ee4314bcc
manish <serverryzen@gmail.com>
2020-09-30 22:20:42 +0530

    fix

--- cron.sh
+++ cron.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "Time: $(date). Some error info." >> /home/manish/cron.log
-sudo docker-compose restart >> /home/manish/cron.log 2>&1
-sleep 10
-curl -q https://aiapi-1.exweb.in/datasync/full?account-name=devrecruit
-curl -q https://aiapi-1.exweb.in/datasync/full?account-name=excellencerecruit
-curl -q https://aiapi-1.exweb.in/datasync/full?account-name=rocketrecruit
+#sudo docker-compose restart >> /home/manish/cron.log 2>&1
+#sleep 10
+#curl -q https://aiapi-1.exweb.in/datasync/full?account-name=devrecruit
+#curl -q https://aiapi-1.exweb.in/datasync/full?account-name=excellencerecruit
+#curl -q https://aiapi-1.exweb.in/datasync/full?account-name=rocketrecruit
 #sudo docker-compose restart searchindexmq
 echo "Time: $(date). Some error info." >> /home/manish/cron.log
 #sudo docker-compose restart
