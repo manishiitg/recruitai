cbf3c78059e8868378ea78ba2d1310b1172ad589
manish <serverryzen@gmail.com>
2020-10-04 16:37:57 +0530

    fix

--- cron.sh
+++ cron.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo "Time: $(date). Some error info." >> /home/manish/cron.log
-#sudo docker-compose restart >> /home/manish/cron.log 2>&1
+sudo docker-compose restart imagemq  >> /home/manish/cron.log 2>&1
+sudo docker-compose restart resumemq >> /home/manish/cron.log 2>&1
+sudo docker-compose restart summarymq >> /home/manish/cron.log 2>&1
 #sleep 10
 #curl -q https://aiapi-1.exweb.in/datasync/full?account-name=devrecruit
 #curl -q https://aiapi-1.exweb.in/datasync/full?account-name=excellencerecruit
