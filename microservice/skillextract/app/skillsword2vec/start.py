cc19c51b1653f09168c4580c3c4a1754a7d53894
manish <serverryzen@gmail.com>
2020-10-12 15:52:38 +0530

    fix

--- microservice/skillextract/app/skillsword2vec/start.py
+++ microservice/skillextract/app/skillsword2vec/start.py
@@ -1,7 +1,44 @@
 from app.config import BASE_PATH
 from gensim.models import Word2Vec
 from app.logging import logger
+import glob
 
+globalModel = {}
+domainList = []
+
+def getDomainModel(domain = None):
+    global globalModel
+    global domainList
+    logger.critical("domain: %s", domain)
+    if not domain:
+        return loadModel()
+    if domain == "others":
+        globalModel[domain] = loadModel()
+    
+    # if globalModel is None:
+    #     logger.info("loading model... %s" , "/workspace/word2vec/word2vec/work2vecskillfull.bin")
+    #     model = Word2Vec.load("/workspace/word2vec/word2vec/work2vecskillfull.bin")
+    if domain in globalModel:
+        return globalModel[domain]
+    else:
+        return loadModel()
+
+def loadDomainModel(domain = None):
+    global globalModel
+    global domainList
+    
+    models = glob.glob("/workspace/word2vec/word2vec/*.bin")
+    logger.critical(models)
+    for model in models:
+        if "-" not in model:
+            continue
+        else:
+            domain = model.split("-")[1]
+            domainList.append(domain)
+            globalModel[domain] = Word2Vec.load(model)
+    return globalModel
+    
+        
 
 model = None
 def loadModel():
