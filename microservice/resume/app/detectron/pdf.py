1e311fa657edcd2fdaa74d5d5c1ac37de13f4047
manish <serverryzen@gmail.com>
2020-10-30 13:54:45 +0530

    fix

--- microservice/resume/app/detectron/pdf.py
+++ microservice/resume/app/detectron/pdf.py
@@ -3,16 +3,43 @@ from pathlib import Path
 import os
 # from pdfminer.high_level import extract_text
 
-
+import fitz
 def covertPDFToImage(cv, output_dir, cvfilename, logger):
-    pages = convert_from_path(cv)
+
+    logger.debug("creating output dir %s", output_dir)
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    doc = fitz.open(cv)  # open document
+    pages = len(doc)
+    if len(pages) >= 20:
+        raise Exception("too many pages not a cv " + str(len(pages)))
+
+    for i, page in enumerate(doc):  # iterate through the pages
+        pix = page.getPixmap(alpha = False)  # render page to an image
+        subpagecvfilename = os.path.join(
+            "", output_dir, "page" + str(i) + '.png')
+        logger.debug("saving cv images to %s", subpagecvfilename)
+
+        pix.writePNG(subpagecvfilename)  # store image as a PNG
+        if i > 5:
+            break
+
+    return
+    # this is giving poppler error randomly. so using another library for testing 
+    pages = 1
+    try:
+        pages = convert_from_path(cv)
+    except expression as identifier:
+        pass
 
     if len(pages) >= 20:
         raise Exception("too many pages not a cv " + str(len(pages)))
 
+
     
-    logger.debug("creating output dir %s", output_dir)
-    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    
+    
     # cvnonum = ''.join(e for e in cvfilename if e.isalnum())
     logger.debug("len of pages %s", len(pages))
     for i, page in enumerate(pages):
