662b40a7df3350de8085c4befbc3d65637114930
manish <serverryzen@gmail.com>
2020-11-22 17:22:15 +0530

    fix

--- microservice/qafull/app/main.py
+++ microservice/qafull/app/main.py
@@ -533,13 +533,15 @@ class ReconnectingTaskQueue(object):
 
 import time
 import subprocess
+import torch
+
 def main():     
     is_gpu_mandatory = int(os.getenv("GPU_MANDATORY", 0))
     if is_gpu_mandatory == 1:
         if not torch.cuda.is_available():
             LOGGER.critical("gpu is not there cannot start without it as it is mandatory")
             return
-            
+
     loadModel()
 
     result = subprocess.run(['gsutil', '-m', 'cp', '-rn',
