f6dcb2c77cbc490931c8f25ba0cab7fa537f25ad
manish <serverryzen@gmail.com>
2020-11-22 11:58:25 +0530

    fix

--- microservice/qafull/app/qa/util.py
+++ microservice/qafull/app/qa/util.py
@@ -56,6 +56,23 @@ def clean_page_content_map(idx, page_contents):
 
         new_lines.append(line)
 
+    cleanLineData = []
+    for line in new_lines:
+        # line = re.sub('\s+', ' ', line).strip() # this is giving warning on server
+        # line = ' '.join(line.split())
+
+        len_words = 0
+        for word in list(filter(None, line.split(' '))):
+            if len(word) > len_words:
+                len_words = len(word)
+
+        if len_words == 1 and len(list(filter(None, line.split(' ')))) > 2:
+            # print("some issue with line %s", line)
+            line = "".join(line.split(" "))
+            # print("new line %s", line)
+
+        cleanLineData.append(line)
+
     line_without_space = 0
     for line in cleanLineData:
         if len(line.strip()) == 0:
