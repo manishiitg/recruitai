dc20e47b5c25312bf9f4cd57d9f04d1bcbf57e7f
manish <serverryzen@gmail.com>
2020-11-29 10:48:41 +0530

    fix

--- microservice/qafull/app/logging.py
+++ microservice/qafull/app/logging.py
@@ -1,13 +1,13 @@
 import logging
 import sys
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.CRITICAL)
 logger = logging.getLogger(__name__)
 
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
+# handler = logging.StreamHandler(sys.stdout)
+# handler.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# handler.setFormatter(formatter)
 
-logger.addHandler(handler)
-# 
+# logger.addHandler(handler)
+# # 
