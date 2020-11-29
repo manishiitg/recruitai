adb0931a93bc458ad03f00883108933834e4cf3d
manish <serverryzen@gmail.com>
2020-11-29 09:40:13 +0530

    fix

--- microservice/filter/app/logging.py
+++ microservice/filter/app/logging.py
@@ -1,12 +1,12 @@
 import logging
 import sys
 
-logging.basicConfig(level=logging.CRITICAL)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# handler = logging.StreamHandler(sys.stderr)
-# handler.setLevel(logging.CRITICAL)
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# handler.setFormatter(formatter)
+handler = logging.StreamHandler(sys.stderr)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
 
-# logger.addHandler(handler)
+logger.addHandler(handler)
