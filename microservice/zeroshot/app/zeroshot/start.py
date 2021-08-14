54e9a84dfbb70df8e489297a3d48ea3856f8d820
manishiitg <serverryzen@gmail.com>
2021-08-14 22:04:12 +0530

    huggingface inference added to zeroshot

--- microservice/zeroshot/app/zeroshot/start.py
+++ microservice/zeroshot/app/zeroshot/start.py
@@ -1,3 +1,17 @@
+from app.account import initDB, get_cloud_bucket, connect_redis
+import subprocess
+import sys
+from pathlib import Path
+import traceback
+import time
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+import os
+from app.config import BASE_PATH
+from app.publishdatasync import sendMessage as datasync
+import requests
+import json
+import datetime
 from transformers import pipeline
 import torch
 from app.logging import logger
@@ -8,42 +22,28 @@ model = None
 tokenizer = None
 zeroshot = None
 zeroshot_fast = None
-import datetime
 
-from app.publishdatasync import sendMessage as datasync
 
 # from pdfminer.pdfpage import PDFTextExtractionNotAllowed
 # from pdfminer.high_level import extract_text
 
-from app.config import BASE_PATH
-import os
-from pymongo import MongoClient
-from bson.objectid import ObjectId
-import time
-import traceback
 
-from app.account import initDB, get_cloud_bucket , connect_redis
-
-from pathlib import Path
-
-import sys
 # from app.config import storage_client
-import subprocess
-import requests
 
-def process(text, labels, mongoid, notifyurl, priority, account_name, account_config, meta = {}):
-    
-    max_label, scores, labels =  classifyZeroShot(text, labels, priority)
+
+def process(text, labels, mongoid, notifyurl, priority, account_name, account_config, meta={}):
+
+    max_label, scores, labels = classifyZeroShot(text, labels, priority)
 
     if notifyurl:
         logger.critical("notify url found %s", notifyurl)
-        x = requests.post(notifyurl, data = {
+        x = requests.post(notifyurl, data={
             'text': text,
-            "labels" : labels,
-            "mongoid" : mongoid,
-            "max_label" : max_label,
-            "labels" : labels,
-            "meta" : meta
+            "labels": labels,
+            "mongoid": mongoid,
+            "max_label": max_label,
+            "labels": labels,
+            "meta": meta
         })
         logger.critical(x)
 
@@ -52,31 +52,31 @@ def process(text, labels, mongoid, notifyurl, priority, account_name, account_co
         db = initDB(account_name, account_config)
 
         row = db.emailStored.find_one({
-                "_id" : ObjectId(mongoid)
-            })
+            "_id": ObjectId(mongoid)
+        })
 
         if not row:
             logger.critical("mongo id not found")
-            return {"error" : "mongo id not found"}
+            return {"error": "mongo id not found"}
 
         db.emailStored.update_one({
-            "_id" : ObjectId(mongoid)
+            "_id": ObjectId(mongoid)
         }, {
-            "$set" : {
-                "cvParsedInfo.zeroshot" : {
-                    "text" : text,
-                    "labels" : labels,
-                    "max_label" : max_label,
-                    "scores" : scores,
-                    "time" : datetime.datetime.now()
+            "$set": {
+                "cvParsedInfo.zeroshot": {
+                    "text": text,
+                    "labels": labels,
+                    "max_label": max_label,
+                    "scores": scores,
+                    "time": datetime.datetime.now()
                 }
             }
         })
 
     return max_label
-            
 
-def classifyZeroShot(text, labels, priority):
+
+def classifyZeroShot(text, labels, priority, api=True):
     if len(text) >= 256:
         text = text[:256]
 
@@ -84,9 +84,31 @@ def classifyZeroShot(text, labels, priority):
 
     logger.critical("text :%s", text)
     logger.critical("labels :%s", labels)
+    if api:
+        API_TOKEN = os.getenv("HUGGINGFACE_INTERFACE_API", "")
+        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"
+        headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-    classify = zeroshot(text, labels)   
-    scores = classify["scores"] 
+        def query(payload):
+            data = json.dumps(payload)
+            response = requests.request(
+                "POST", API_URL, headers=headers, data=data)
+            return json.loads(response.content.decode("utf-8"))
+
+        classify = query({
+            "inputs": text,
+            "parameters": {
+                "candidate_labels": labels
+            },
+        })
+
+        print(classify)
+        if "error" in classify:
+            return classifyZeroShot(text, labels, priority, False)
+    else:
+        classify = zeroshot(text, labels)
+
+    scores = classify["scores"]
     labels = classify["labels"]
 
     logger.critical("labels :%s", labels)
@@ -101,33 +123,31 @@ def classifyZeroShot(text, labels, priority):
 
     logger.critical("max_label :%s", max_label)
     return max_label, scores, labels
-    
+
     # except Exception as e:
     #     logger.critical(e)
     #     return None
-    
 
-
-from pathlib import Path
 
 def loadModel():
     global model
     global tokenizer
     global zeroshot
     global zeroshot_fast
-    
+
     logger.critical("gpu %s", torch.cuda.is_available())
     if zeroshot is None:
         if torch.cuda.is_available():
             zeroshot = pipeline("zero-shot-classification", device=0)
         else:
             zeroshot = pipeline("zero-shot-classification")
-    
+
     if zeroshot_fast is None and False:
         if torch.cuda.is_available():
-            zeroshot_fast = pipeline('zero-shot-classification', model='valhalla/distilbart-mnli-12-3')
+            zeroshot_fast = pipeline(
+                'zero-shot-classification', model='valhalla/distilbart-mnli-12-3')
         else:
-            zeroshot_fast = pipeline('zero-shot-classification', model='valhalla/distilbart-mnli-12-3')
-        
-    
+            zeroshot_fast = pipeline(
+                'zero-shot-classification', model='valhalla/distilbart-mnli-12-3')
+
     return model, tokenizer, zeroshot
